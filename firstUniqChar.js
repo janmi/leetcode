065a8e828f9db6f5b841921e5f84c78584af21e3
janmi <627943558@qq.com>
2018-07-30 23:54:09 +0800

    feat add firstUniqChar

---
+++ firstUniqChar.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+  let len = s.length
+  let copyStr = s
+  let result = -1
+  function find () {
+  	let reg = new RegExp(s[0], 'g')
+  	if (s.length === 0 && s === '') return -1
+  	if (s.match(reg).length === 1) {
+  		result = copyStr.indexOf(s[0])
+  	} else {
+  		s = s.replace(reg, '')
+  		find()
+  	}
+  }
+  find()
+  return result
+}
+
+
+
+console.time('time:')
+console.log(firstUniqChar(""))
+console.timeEnd('time:')
