e0696048ee592f8c40a715e8f46b9cdf212da6ff
janmi <627943558@qq.com>
2021-04-29 23:19:28 +0800

    feat: 增加解法

--- deleteNode.js
+++ deleteNode.js
@@ -32,3 +32,19 @@ var deleteNode = function(head, val) {
   }
   return copy[0];
 };
+
+// 第二种采用指针算法，比第一种更简洁，但是时间相差不大，线性的
+var deleteNode = function(head, val) {
+  if (head.val === val) return head.next
+  let prev = head;
+  let cur = head.next;
+  while (cur) {
+    if (cur.val === val) {
+      prev.next = cur.next;
+      cur = null;
+      return head;
+    }
+    prev = cur;
+    cur = cur.next;
+  }
+}
