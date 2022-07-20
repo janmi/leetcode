0032712a6583cbaf30fb8dc3e1d70ac34fe12536
zhuxiaofeng <zhuxiaofeng@kp99.cn>
2022-07-20 15:03:55 +0800

    refactor: 优化代码

--- balancedStringSplit.js
+++ balancedStringSplit.js
@@ -5,15 +5,15 @@
 var balancedStringSplit = function(s) {
 	if (s.length === 2) return [s];
 
-	const res = [];
 	let index = 0;
 	let len = 2;
+	let count = 0;
 
 	while ((index + len) <= s.length) {
 		let child = s.substr(index, len);
 
-		if (child.match(/R/g) && child.length / child.match(/R/g).length === 2) {
-			res.push(child);
+		if ((child.length / child.replace(/L/g, '').length) === 2) {
+			count++
 
 			index = index + len;
 			len = 2;
@@ -22,7 +22,7 @@ var balancedStringSplit = function(s) {
 		}
 	}
 
-	return res.length;
+	return count;
 };
 
 balancedStringSplit('RRLL')
